4b42884e051593811c8f09257a1c09d9f60b3dda
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-16 22:55:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (let i = 0, l = testArray.length; i < l; i++) {
+function forLoop(array){
+for (let i = 0, l = array.length; i < l; i++) {
         let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+  }
 }
