6d5ebb8bc824aab8a0b798af2d2117a8484dd759
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-16 23:16:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,6 +14,14 @@ function whileLoop(n){
   return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length > 0 && maybeTrue())
   
+  return array;
 }
